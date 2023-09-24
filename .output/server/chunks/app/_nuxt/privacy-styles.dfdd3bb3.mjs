f08809c2b745fc35474c4d463dd565950381b75d
susc <szuzsc@live.com>
2023-09-24 20:09:59 +0800

    删除.output文件夹

--- .output/server/chunks/app/_nuxt/privacy-styles.dfdd3bb3.mjs
+++
@@ -1,6 +0,0 @@
-const privacy_vue_vue_type_style_index_0_scoped_dab39af1_lang = ".container[data-v-dab39af1]{margin-top:3rem}";
-
-const privacyStyles_dfdd3bb3 = [privacy_vue_vue_type_style_index_0_scoped_dab39af1_lang, privacy_vue_vue_type_style_index_0_scoped_dab39af1_lang];
-
-export { privacyStyles_dfdd3bb3 as default };
-//# sourceMappingURL=privacy-styles.dfdd3bb3.mjs.map
--- .output/server/chunks/app/_nuxt/terms-and-conditions-styles.1dfdb54f.mjs
+++
@@ -1,6 +0,0 @@
-const termsAndConditions_vue_vue_type_style_index_0_scoped_3d8abe7d_lang = ".container[data-v-3d8abe7d]{margin-top:3rem}";
-
-const termsAndConditionsStyles_1dfdb54f = [termsAndConditions_vue_vue_type_style_index_0_scoped_3d8abe7d_lang, termsAndConditions_vue_vue_type_style_index_0_scoped_3d8abe7d_lang];
-
-export { termsAndConditionsStyles_1dfdb54f as default };
-//# sourceMappingURL=terms-and-conditions-styles.1dfdb54f.mjs.map
